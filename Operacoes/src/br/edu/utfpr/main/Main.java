9a5297c4d685bd9a66a2ef208661c6f939fa0390
rafael.stuzata <rafael.stuzata@flowanalytics.com.br>
2026-03-18 22:28:47 -0300

    metodos construtores e operações

--- Operacoes/src/br/edu/utfpr/main/Main.java
+++ Operacoes/src/br/edu/utfpr/main/Main.java
@@ -9,16 +9,23 @@ import br.edu.utpfr.operacoes.Operacoes;
 public class Main {
 
     public static void main(String[] args) {
-        Operacoes op = new Operacoes();
+        System.out.println("operacoes1");
+        Operacoes op1 = new Operacoes();
+        op1.setNum1(15);
+        op1.setNum2(9);
+        System.out.println(op1);
+        System.out.println("soma: " + op1.somar());
+        System.out.println("subtrai: " + op1.subtrair());
+        System.out.println("multiplica: " + op1.multiplicar());
+        System.out.println("divide: " + op1.dividir());
 
-        op.setNum1(9);
-
-        System.out.println(op.getNum1());
-
-        op.setNum2(9);
-
-        System.out.println(op.getNum2());
-
+        System.out.println("\noperacoes2");
+        Operacoes op2 = new Operacoes(5, 2);
+        System.out.println(op2);
+        System.out.println("soma: " + op2.somar());
+        System.out.println("subtrai: " + op2.subtrair());
+        System.out.println("multiplica: " + op2.multiplicar());
+        System.out.println("divide: " + op2.dividir());
     }
 
 }
--- Operacoes/src/br/edu/utpfr/operacoes/Operacoes.java
+++ Operacoes/src/br/edu/utpfr/operacoes/Operacoes.java
@@ -8,9 +8,20 @@ public class Operacoes {
 
     private int num1, num2;
 
+    //metodo construtor padrao
+    public Operacoes() {
+    }
+
+    public Operacoes(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
     //acessores (get) e modificadores (set)
     public void setNum1(int num1) {
-        this.num1 = num1;
+        if (num1 >= 0) {
+            this.num1 = num1;
+        }
     }
 
     public int getNum1() {
@@ -23,5 +34,26 @@ public class Operacoes {
 
     public int getNum2() {
         return num2;
+    }
+
+    public int somar() {
+        return num1 + num2;
+    }
+
+    public int subtrair() {
+        return num1 - num2;
+    }
+
+    public int multiplicar() {
+        return num1 * num2;
+    }
+
+    public float dividir() {
+        return (float) num1 / (float) num2;
+    }
+
+    @Override
+    public String toString() {
+        return "número1: " + num1 + "\nnúmero2: " + num2;
     }
 }
