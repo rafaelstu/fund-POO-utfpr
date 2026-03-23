717c34ccc8ea546aba22bd1e7ab59040010cc802
rafael.stuzata <rafael.stuzata@flowanalytics.com.br>
2026-03-22 22:53:22 -0300

    exercicio imc

---
+++ Imc/src/br/edu/utfpr/imc/Imc.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.utfpr.imc;
+
+/**
+ *
+ * @author tomate
+ */
+public class Imc {
+
+    private double peso, altura;
+
+    public Imc() {
+    }
+
+    public Imc(double peso, double altura) {
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double calcularImc() {
+        if (peso == 0 || altura == 0) {
+            return 0;
+        }
+        return peso / (altura * altura);
+    }
+
+    public String classificacaoImc(double valorImc) {
+        if (valorImc < 20) {
+            return "magro";
+        }
+        if (valorImc < 24) {
+            return "normal";
+        }
+        if (valorImc < 29) {
+            return "acima do peso";
+        }
+        if (valorImc < 34) {
+            return "obeso";
+        }
+        return "muito obeso";
+    }
+
+    @Override
+    public String toString() {
+        return "peso: " + peso + "\naltura: " + altura;
+    }
+}
---
+++ Imc/src/br/edu/utfpr/main/Main.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package br.edu.utfpr.main;
+
+import br.edu.utfpr.imc.Imc;
+import java.util.Scanner;
+
+/**
+ *
+ * @author tomate
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Imc imc = new Imc();
+        double imcCalc;
+
+        Imc imcCodigo = new Imc(69, 1.75);
+        double imcCalcCodigo = imcCodigo.calcularImc();
+        System.out.println("valores imc definidos na inicialização: " + imcCodigo.toString());
+        System.out.println("calculo do imc definido na inicialização: " + imcCalcCodigo);
+        System.out.println("classificação imc definido na inicialização: " + imcCodigo.classificacaoImc(imcCalcCodigo));
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("informe o peso (kg): ");
+        imc.setPeso(input.nextDouble());
+        System.out.println("informe a altura (metros): ");
+        imc.setAltura(input.nextDouble());
+        System.out.println("valores imc definidos pelo usuario: " + imc.toString());
+
+        imcCalc = imc.calcularImc();
+        System.out.println("calculo do imc definido pelo usuario: " + imcCalc);
+        System.out.println("classificação imc definido pelo usuario: " + imc.classificacaoImc(imcCalc));
+
+    }
+
+}
