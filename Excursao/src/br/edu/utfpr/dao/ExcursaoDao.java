02af8add2724a7c15125f29e2c9fd166bf634e3d
rafael.stuzata <rafael.stuzata@flowanalytics.com.br>
2026-05-06 21:42:36 -0300

    excursao

--- Excursao/src/br/edu/utfpr/dao/ExcursaoDao.java
+++ Excursao/src/br/edu/utfpr/dao/ExcursaoDao.java
@@ -4,7 +4,10 @@
  */
 package br.edu.utfpr.dao;
 
+import br.edu.utfpr.model.Excursao;
 import br.edu.utfpr.model.Passageiro;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -12,4 +15,54 @@ import br.edu.utfpr.model.Passageiro;
  */
 public class ExcursaoDao {
 
+    private List<Passageiro> todosPassageiros;
+
+    public ExcursaoDao() {
+        this.todosPassageiros = new ArrayList<>();
+    }
+
+    // Adiciona passageiro à excursão e à lista geral
+    public void addPassageiro(Passageiro passageiro, Excursao excursao) {
+        if (passageiro != null && excursao != null) {
+            excursao.getPassageiros().add(passageiro);
+            todosPassageiros.add(passageiro);
+        }
+    }
+
+    // Imprime passageiros de uma excursão específica
+    public String imprimirListaPassageiro(Excursao excursao) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Lista de Passageiros:\n");
+        for (Passageiro p : excursao.getPassageiros()) {
+            sb.append(p).append("\n");
+        }
+        return sb.toString();
+    }
+
+    // Imprime todos os passageiros cadastrados (lista geral)
+    public String imprimirListaPassageiro() {
+        StringBuilder sb = new StringBuilder();
+        for (Passageiro p : todosPassageiros) {
+            sb.append(p).append("\n");
+        }
+        return sb.toString();
+    }
+
+    // Remove passageiro de uma excursão pelo id; retorna true se removeu
+    public boolean excluirPassageiroExcursao(Excursao excursao, int idItem) {
+        if (excursao == null || excursao.getPassageiros() == null) {
+            return false;
+        }
+        for (Passageiro p : excursao.getPassageiros()) {
+            if (p.getId() == idItem) {
+                excursao.getPassageiros().remove(p);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<Passageiro> getTodosPassageiros() {
+        return todosPassageiros;
+    }
 }
--- Excursao/src/br/edu/utfpr/main/Main.java
+++ Excursao/src/br/edu/utfpr/main/Main.java
@@ -4,6 +4,13 @@
  */
 package br.edu.utfpr.main;
 
+import br.edu.utfpr.dao.ExcursaoDao;
+import br.edu.utfpr.model.Excursao;
+import br.edu.utfpr.model.Guia;
+import br.edu.utfpr.model.Passageiro;
+import br.edu.utfpr.model.Veiculo;
+import java.time.LocalDate;
+
 /**
  *
  * @author tomate
@@ -14,7 +21,67 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Guia guia1 = new Guia(1234, "(46) 3225-0909", "Lucia Ferreira");
+        Guia guia2 = new Guia(5678, "(46) 9888-1234", "Marcos Alves");
+
+        Veiculo van = new Veiculo("Van", "ABC-8989", 12);
+        Veiculo onibus = new Veiculo("Ônibus", "XYZ-1234", 40);
+
+        Excursao exc1 = new Excursao("Fernando de Noronha",
+                LocalDate.now(), guia1, van, null);
+
+        Excursao exc2 = new Excursao("Chapada dos Veadeiros",
+                LocalDate.now(), guia2, onibus, null);
+
+        ExcursaoDao dao = new ExcursaoDao();
+
+        dao.addPassageiro(new Passageiro("123345", 25, "Carlos Silva"), exc1);
+        dao.addPassageiro(new Passageiro("234456", 30, "Ana Souza"), exc1);
+        dao.addPassageiro(new Passageiro("345567", 28, "Beatriz Oliveira"), exc1);
+        dao.addPassageiro(new Passageiro("456678", 35, "Felipe Costa"), exc1);
+
+        dao.addPassageiro(new Passageiro("567890", 22, "Juliana Almeida"), exc2);
+        dao.addPassageiro(new Passageiro("678901", 31, "Roberto Silva"), exc2);
+        dao.addPassageiro(new Passageiro("789012", 27, "Mariana Costa"), exc2);
+        dao.addPassageiro(new Passageiro("890123", 29, "Thiago Pereira"), exc2);
+
+        System.out.println("--------------------Excursão 1 --------------------------");
+        System.out.println("Destino: " + exc1.getDestino());
+        System.out.println("Data: " + exc1.getData());
+        System.out.println(exc1.getGuiaResponsavel());
+        System.out.println(exc1.getVeiculo());
+        System.out.println();
+        System.out.print(dao.imprimirListaPassageiro(exc1));
+
+        System.out.println("\n--------------------Excursão 2 --------------------------");
+        System.out.println("Destino: " + exc2.getDestino());
+        System.out.println("Data: " + exc2.getData());
+        System.out.println(exc2.getGuiaResponsavel());
+        System.out.println(exc2.getVeiculo());
+        System.out.println();
+        System.out.print(dao.imprimirListaPassageiro(exc2));
+
+        System.out.println("\n---------------Lista de todos os passageiros -------------------");
+        System.out.print(dao.imprimirListaPassageiro());
+
+        System.out.println("\nEXCLUIR PASSAGEIRO");
+        boolean removeu = dao.excluirPassageiroExcursao(exc2, 6);
+        if (removeu) {
+            System.out.println("Passageiro excluído com sucesso.");
+        } else {
+            System.out.println("Passageiro não encontrado.");
+        }
+
+        System.out.println("------------Lista de passageiros atualizada--------------");
+        System.out.println("Destino: " + exc2.getDestino());
+        System.out.println("Data: " + exc2.getData());
+        System.out.println(exc2.getGuiaResponsavel());
+        System.out.println(exc2.getVeiculo());
+        System.out.println();
+        System.out.println("Lista de Passageiros:");
+        for (Passageiro p : exc2.getPassageiros()) {
+            System.out.println("ID: " + p.getId() + ", " + p);
+        }
     }
 
 }
